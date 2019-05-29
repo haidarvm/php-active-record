72c888f8c68947a6f3d4ebce5a0ae26db0a75fed
Kis György <kisgyprog@gmail.com>
2019-05-29 14:21:13 +0200

    Update DB_driver.php
    Replace echo $message(array) to print_r($message)

--- src/database/DB_driver.php
+++ src/database/DB_driver.php
@@ -1696,7 +1696,7 @@ abstract class CI_DB_driver {
 
 //		$error =& load_class('Exceptions', 'core');
 //		echo $error->show_error($heading, $message, 'error_db');
-		echo $message;
+		print_r($message);
 		exit(8); // EXIT_DATABASE
 	}
 
